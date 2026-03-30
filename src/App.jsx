e9625466bbef97a9403116a1a58846f7e1049578
Amit Sahoo <amitkusahoo1212@gmail.com>
2026-03-31 00:33:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FaceExpression from './features/components/pages/FaceExpression'
+
+const App = () => {
+  return (
+    <main>
+      <FaceExpression/>
+    </main>
+  )
+}
+
+export default App
